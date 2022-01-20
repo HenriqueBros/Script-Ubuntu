79ad008b1de0a197f8d255c94052235f6744a345
HenriqueBros <58406947+HenriqueBros@users.noreply.github.com>
2022-01-20 18:06:54 -0300

    01

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# chmod a+x nome_arquivo
+# ATT
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Node
+
+sudo apt install node
+
+# Snap
+
+sudo apt install snap
+
+# Vscode
+
+sudo snap install code --classic
+
+
