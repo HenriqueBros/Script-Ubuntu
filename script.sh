94c457ba2952e3bbf4baa81245baf60a3b638210
Henrique <hmbroseghini@hotmail.com>
2022-02-12 16:45:51 -0300

    .net-neofetch

--- script.sh
+++ script.sh
@@ -23,4 +23,27 @@ sudo apt install scrcpy -y
 
 sudo snap install notion -y
 
+# .net
+
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+# sdk
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+  
+#runtime
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-6.0
+  
+# neofetch
+
+sudo apt install neofetch
 
