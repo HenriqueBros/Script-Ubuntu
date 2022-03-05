75a81a59714a999747be79f58cc1a16b92ce2f9f
HenriqueBros <hmbroseghini@hotmail.com>
2022-03-05 12:57:15 -0300

    add-npm-docker

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 # Node
 sudo apt-get install nodejs -y
-
+sudo apt-get install npm -y
 # Snap
 sudo apt-get install snap -y
 
@@ -13,27 +13,32 @@ sudo apt-get install snap -y
 sudo snap install code --classic
 
 # Scrcpy
-sudo apt install scrcpy -y
+# sudo apt install scrcpy -y
 
 # Notion
 sudo snap install notion -y
 
+# docker
+sudo apt-get install docker.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # .net
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
+# wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+# rm packages-microsoft-prod.deb
 
 # sdk
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
+#sudo apt-get update; \
+#  sudo apt-get install -y apt-transport-https && \
+#  sudo apt-get update && \
+#  sudo apt-get install -y dotnet-sdk-6.0
   
 #runtime
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y aspnetcore-runtime-6.0
+#sudo apt-get update; \
+#  sudo apt-get install -y apt-transport-https && \
+#  sudo apt-get update && \
+#  sudo apt-get install -y aspnetcore-runtime-6.0
   
 # neofetch
 sudo apt install neofetch -y
