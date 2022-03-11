d8e4276ef7459206072914e6afdc27c21c9cbbfa
HenriqueBros <hmbroseghini@hotmail.com>
2022-03-10 21:30:43 -0300

    figma

--- script.sh
+++ script.sh
@@ -21,6 +21,9 @@ sudo snap install telegram-desktop
 # Notion
 sudo snap install notion-snap
 
+# Figma
+sudo snap install figma-linux
+
 # docker
 sudo apt-get install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
