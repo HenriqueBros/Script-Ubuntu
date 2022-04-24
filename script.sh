e567fc216ba7932fad496f790ce284945b82c816
HenriqueBros <hmbroseghini@hotmail.com>
2022-04-23 23:25:37 -0300

    add-mqttx

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # chmod a+x nome_arquivo
 # ATT
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # Node
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
+sudo apt install nodejs -y
+sudo apt install npm -y
 # Snap
-sudo apt-get install snap -y
+sudo apt install snap -y
 
 # Vscode
 sudo snap install code --classic
@@ -16,13 +16,16 @@ sudo snap install code --classic
 # sudo apt install scrcpy -y
 
 #telegram
-sudo snap install telegram-desktop
+sudo snap install telegram-desktop -y
 
 # Notion
-sudo snap install notion-snap
+sudo snap install notion-snap -y
 
 # Figma
-sudo snap install figma-linux
+sudo snap install figma-linux -y
+
+#MQTT
+sudo snap install mqttx -y
 
 # docker
 sudo apt-get install docker.io -y
@@ -30,7 +33,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 # .net
-# wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# wget__https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 # sudo dpkg -i packages-microsoft-prod.deb
 # rm packages-microsoft-prod.deb
 
